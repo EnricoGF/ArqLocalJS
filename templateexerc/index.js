529f145b1420c4ec4bff9735c916689d084c9f98
EnricoGF <166620038+EnricoGF@users.noreply.github.com>
2024-04-16 09:04:05 -0300

    [new] template exercicos
    template de exercicios feitos

---
+++ templateexerc/index.js
@@ -0,0 +1,39 @@
+/*
+const nome = "enrico"
+const idade = 19
+
+console.log("meu nome é", nome, "\nminha idade é", idade)
+
+*/
+
+/*
+var nome = prompt("seu nome:")
+console.log("seu nome é:\n",nome)
+*/
+
+/*
+const nome = "enrico"
+const sobrenome = "forte"
+const idade = 19
+const estud = false
+
+console.log("meu nome é", nome , sobrenome, "tenho", idade, "anos e", estud)
+*/
+
+/*
+const nome = prompt("seu nome:")
+const idade = prompt("sua idade:")
+
+console.log(nome)
+console.log(idade)
+
+console.log("Nome do usuario", typeof nome)
+console.log("Idade do usuario", typeof idade)
+
+*/
+
+
+const idadetext = prompt("sua idade:")
+console.log(idadetext)
+const idadenumber = Number(idadetext)
+console.log(idadenumber)
