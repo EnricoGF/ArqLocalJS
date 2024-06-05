53a5fd1efaf1a82ce6894989d7ddf5ed7b65a334
EnricoGF <166620038+EnricoGF@users.noreply.github.com>
2024-06-04 21:52:01 -0300

    [updt] index.js

--- desafio/index.js
+++ desafio/index.js
@@ -6,6 +6,7 @@ Categoria: pode ser as opções 1, 2, 3 ou 4;
 Quantidade de ingressos.
 */
 
+//tabela de valores dos ingressos domesticos
 const tabelaValores = {
     SF: [ 1320, 880, 550, 220 ],
     DT: [ 660, 440, 330, 170 ],
@@ -19,27 +20,40 @@ const etapaJogo = prompt(`Selecione a etapa do jogo entre SF para Semi-final, DT
 const categoria = Number(prompt(`Selecione a categoria entre 1, 2, 3 ou 4`)) - 1
 const ingressoUser = Number(prompt(`Selecione a quantidade de ingressos`))
 let ingressoValor
+let ingressoInfo
+let valorTotalInfo
 
+//definindo o valor do ingresso a partir do tipo do jogo
 if(tipoJogo===`DO`){
-    ingressoValor = tabelaValores[etapaJogo][categoria]
+     ingressoValor = tabelaValores[etapaJogo][categoria]
+     ingressoInfo = `R$ ${ingressoValor}`
 }
 else if(tipoJogo===`IN`){
-    ingressoValor = tabelaValores[etapaJogo][categoria]/5
+     ingressoValor = tabelaValores[etapaJogo][categoria]/5
+     ingressoInfo = `U$ ${ingressoValor}`
 }
 
 let valorTotal = ingressoUser*ingressoValor
 
-//função printa informações no console
+//transformando valores no formato final das informações
+if(tipoJogo===`DO`){ 
+    valorTotalInfo = `R$ ${valorTotal}`
+}
+else if(tipoJogo===`IN`){
+    valorTotalInfo = `U$ ${valorTotal}`
+}
+
+//função para printar informações no console
 function printInfo (){
     console.log(`---Dados da compra---\n
     Nome do cliente: ${nomeUser}\n
     Tipo do jogo: ${tipoJogo}\n
     Etapa do jogo: ${etapaJogo}\n
-    Categoria: ${categoria}\n
-    Quantidade de Ingressos: ${ingressosUser}\n
+    Categoria: ${categoria+1}\n
+    Quantidade de Ingressos: ${ingressoUser}\n
     ---Valores---\n
-    Valor do ingresso:  R$ 1.980\n
-    Valor total: ${valorTotal}`)
+    Valor do ingresso:  ${ingressoInfo}\n
+    Valor total: ${valorTotalInfo}`)
 }
 
 printInfo()
